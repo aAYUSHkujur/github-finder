195650eb47fd1f69c51aa6c753f5f3e5d546817d
aayushk720@gmail.com <aayushk720@gmail.com>
2023-06-14 17:23:29 +0530

    removing token

--- src/Components/users/UserResults.js
+++ src/Components/users/UserResults.js
@@ -11,11 +11,7 @@ const UserResults = () => {
   }, []);
 
   const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-      },
-    });
+    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`);
 
     const data = await response.json();
     setUsers(data);
